8056a66890f7907b3e70685f404965c36774ea20
yassinehaimouch <haymouch53@gmail.com>
2023-05-19 23:46:04 +0100

    Solve Exercise 1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,37 @@
-// 🐨 you'll need to import react and createRoot from react-dom up here
+import React from 'react'
+import {useState} from 'react'
+import {Logo} from 'components/logo'
+import {Dialog} from '@reach/dialog'
+import '@reach/dialog/styles.css'
+import {createRoot} from 'react-dom/client'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+function App() {
+  const [openModal, setOpenModal] = useState('none')
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
-
-// 🐨 use createRoot to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={() => setOpenModal('login')}>Login</button>
+      </div>
+      <div>
+        <button onClick={() => setOpenModal('register')}>Register</button>
+      </div>
+      <Dialog aria-label="Login from" isOpen={openModal === 'login'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Login</h3>
+      </Dialog>
+      <Dialog aria-label="Registration from" isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Register</h3>
+      </Dialog>
+    </div>
+  )
+}
+const root = document.getElementById('root')
+createRoot(root).render(<App />)
